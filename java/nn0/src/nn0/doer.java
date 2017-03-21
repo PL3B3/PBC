1ce452d38ab7a2e3f3de66de7312d59379caa193
PL3B3 <kevinqiesq@gmail.com>
2017-03-20 19:17:30 -0500

    MORE STOOF

---
+++ java/nn0/src/nn0/doer.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nn0;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author KevinQi19
+ */
+public class doer {
+    private ArrayList<glorytokimilsung> repertoire = new ArrayList<glorytokimilsung>();
+    
+    public doer() {
+        
+    }
+    
+    public glorytokimilsung get_kim(int dex) {
+        return Ar
+    }
+}
---
+++ java/nn0/src/nn0/glorytokimilsung.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nn0;
+
+/**
+ *
+ * @author KevinQi19
+ */
+public class glorytokimilsung {
+    public String snippet = "";
+    
+    public task purpose;
+    
+    public String return_type;
+    
+    public glorytokimilsung(String code) {
+        snippet = code;
+    }
+}
